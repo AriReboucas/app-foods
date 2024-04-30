5b459df74153dd80e52be2182fe1cbc2a8407cec
AriReboucas <arireboucasdev@gmail.com>
2024-04-29 21:13:53 -0300

    feat: add initial header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "./_components/header";
+
 const Home = () => {
   return (
-    <div className="bg-red-500 p-5">
-      <h1>hello tailwind</h1>
+    <div>
+      <Header />
     </div>
   );
 };
