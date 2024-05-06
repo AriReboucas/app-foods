a599e4c21b7de9d363b7d169a361902390268d3b
AriReboucas <arireboucasdev@gmail.com>
2024-05-06 17:54:47 -0300

    feat: ensure order can be creates

---
+++ prisma/migrations/20240505230441_add_order_table_edit/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `deliveryTimeMinutes` on the `Order` table. All the data in the column will be lost.
+  - Added the required column `deliveryTimeMinutes` to the `Order` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterEnum
+ALTER TYPE "OrderStatus" ADD VALUE 'CANCELED';
+
+-- AlterTable
+ALTER TABLE "Order" DROP COLUMN "deliveryTimeMinutes",
+ADD COLUMN     "deliveryTimeMinutes" INTEGER NOT NULL;
