3d96b37bd301573fce41fc32a2466a2eac4b2108
AriReboucas <arireboucasdev@gmail.com>
2024-05-07 21:33:31 -0300

    feat: add favorite restaurants page

--- app/_components/restaurant-item.tsx
+++ app/_components/restaurant-item.tsx
@@ -9,9 +9,9 @@ import Link from "next/link";
 import { cn } from "../_lib/utils";
 import { toggleFavoriteRestaurant } from "../_actions/restaurant";
 import { toast } from "sonner";
+import { useSession } from "next-auth/react";
 
 interface RestaurantItemProps {
-  userId?: string;
   restaurant: Restaurant;
   className?: string;
   userFavoritesRestaurants: UserFavoriteRestaurant[];
@@ -20,18 +20,19 @@ interface RestaurantItemProps {
 const RestaurantItem = ({
   restaurant,
   className,
-  userId,
   userFavoritesRestaurants,
 }: RestaurantItemProps) => {
-  const isFavorite = userFavoritesRestaurants.some(
+  const { data } = useSession();
+
+  const isFavorite = userFavoritesRestaurants?.some(
     (fav) => fav.restaurantId === restaurant.id,
   );
 
   const handleFavoriteClick = async () => {
-    if (!userId) return;
+    if (!data?.user.id) return;
 
     try {
-      await toggleFavoriteRestaurant(userId, restaurant.id);
+      await toggleFavoriteRestaurant(data?.user.id, restaurant.id);
       toast.success(
         isFavorite
           ? "Restaurante removido dos favoritos."
@@ -63,7 +64,7 @@ const RestaurantItem = ({
             <span className="text-xs font-semibold">5.0</span>
           </div>
 
-          {userId && (
+          {data?.user.id && (
             <Button
               size="icon"
               className={`absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700 
